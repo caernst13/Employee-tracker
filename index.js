fe52e7e6958122cd059ff438589481ab87e80f43
caernst13 <caleb_ernst@yahoo.com>
2023-03-17 15:36:58 -0400

    created connection folder and added switch case to determine what user selected

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const inquirer = require('inquirer');
+// const connection = require('./config/connection')
 const initialQuestion = require('./question')
 
 const init = () => {inquirer.prompt(initialQuestion).then((data) => {
     console.log(data)
+    switch (data.action) {
+        case 'View all departments': viewAllDeparments(); break;
+        case 'View all roles': viewAllRoles(); break;
+        case 'View all employees': viewAllEmployees(); break;
+        case 'Add a department': AddDepartment(); break;
+        case 'Add an employee': AddEmployee(); break;
+        case 'Update an employee role': updateEmployee(); break;
+        default: process.exit();
+      };
 })}
 
 init()
--- question/index.js
+++ question/index.js
@@ -2,8 +2,8 @@ const initialQuestion =
 {
     type: 'list',
     name: 'action',
-    message: 'what would you like to do? (use arrow keys)',
-    choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee','Update an employee role']
+    message: 'what would you like to do?',
+    choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee','Update an employee role', 'exit']
 }
 
 module.exports = initialQuestion
